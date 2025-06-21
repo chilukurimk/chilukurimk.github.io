ec8bef64158a3715b3f39ac5b4846a7fca2e27e5
Madhu Kiran Chilukuri <104894376+chilukurimk@users.noreply.github.com>
2025-06-21 16:33:12 +0000

    Aligned Intro section

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Projects from './Projects';
+import { Intro } from './Intro';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <div></div>
-      <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div>
-      <p>
-          Madhu Kiran Chilukuri
-        </p>
-        <p>B.tech in Computer Science and Engineering, Kalasalingam Academy of Research and Education</p>
-        <p>Passionate about software development and technology, Open to collaboration and learning new technologies.</p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/madhu-kiran-chilukuri-003b481a9"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Linkedin 
-        </a>
-        <p>Here are some of my projects that I have worked on:</p>
-      </div>
+      <Intro />
       <Projects />
     </div>
   );
---
+++ src/Intro.js
@@ -0,0 +1,21 @@
+import "./Intro.css"
+export const Intro = () => {
+    return(
+        <div className="Intro">
+            <p className="Name">
+                Madhu Kiran Chilukuri
+            </p>
+            <p>B.tech in Computer Science and Engineering, Kalasalingam Academy of Research and Education</p>
+            <p>Passionate about software development and technology, Open to collaboration and learning new technologies.</p>
+            <a
+                className="App-link"
+                href="https://www.linkedin.com/in/madhu-kiran-chilukuri-003b481a9"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Linkedin 
+            </a>
+            <p>Here are some of my projects that I have worked on:</p>
+      </div>
+    )
+}
